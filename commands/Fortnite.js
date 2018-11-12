da58083b831aef5b019eaf36695d5460a5c48433
marikuana <43219562+marikuana@users.noreply.github.com>
2018-11-12 11:36:53 +0200

    Update Fortnite.js

--- commands/Fortnite.js
+++ commands/Fortnite.js
@@ -5,8 +5,8 @@ require("moment-duration-format");
 
 const fortniteAPI = new Fortnite(
     [
-        "marians22092000@gmail.com",
-        "22092000mar",
+        "",
+        "",
         "MzRhMDJjZjhmNDQxNGUyOWIxNTkyMTg3NmRhMzZmOWE6ZGFhZmJjY2M3Mzc3NDUwMzlkZmZlNTNkOTRmYzc2Y2Y",
         "ZWM2ODRiOGM2ODdmNDc5ZmFkZWEzY2IyYWQ4M2Y1YzY6ZTFmMzFjMjExZjI4NDEzMTg2MjYyZDM3YTEzZmM4NGQ"
     ],
